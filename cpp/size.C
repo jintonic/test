afd036b7f8887382c5bbf330b15dded50f2d0f04
jing.liu <jing.liu@ipmu.jp>
2010-08-10 01:41:17 +0000

    added complicated struct, Jing
    git-svn-id: file:///home/jingliu/home/jing.liu/svn/test@620 a066fb6a-a811-11dc-9a5e-f027034fa78f

--- cpp/size.C
+++ cpp/size.C
@@ -4,8 +4,36 @@
 
 using namespace std;
 
+typedef struct xtimeval {
+   long long tv_sec;
+   long long tv_usec;
+} XTIMEVAL;
+
+typedef struct xtimeval acq_time; ///< from onlhead.h
+
+/**
+ * BUFHEAD: information block included at the beginning of different buffers.
+ * from onlbuf.h
+ */
+typedef struct sort_buffer_header {
+   u_int    data_size; ///< size of buffer (byte) including head
+   u_int    flag;
+   int      run_number;
+   char     nodename[8]; ///< name of computer
+   u_int    server_number;
+   u_int    tko_number;
+   u_int    serial_event_number;
+   acq_time time;
+   u_int    buffer;
+   u_int    scan_num;
+   u_int    scan_bit;
+   u_int    tko_flag; /* indicate using or no-using smps(TKOs) */
+   u_int    tko_error_flag; /* indicate error occured or not */
+} BUFHEAD;
+
 int main()
 {
+  cout<<"size of bufhead: "<<sizeof(BUFHEAD)<<endl;
   cout<<"size of char: "<<sizeof(char)<<endl;
   cout<<"size of short: "<<sizeof(short)<<endl;
   cout<<"size of u_short: "<<sizeof(u_short)<<endl;
